a84534ee2c6e3feeded54f136b190db9d4ea7098
Job Lameck <j.ayuko@alustudent.com>
2026-09-25 14:42:37 +0100

    hospital_admin.sh

--- hospital_admin.sh
+++ hospital_admin.sh
@@ -5,7 +5,7 @@ initialize_system() {
     if [ -d "active_logs" ]; then
         echo "active_logs already exists"
     else
-        echo "Creating active_logs directory..."
+        echo "Creating active_logs directory.."
         mkdir active_logs
     fi
 
